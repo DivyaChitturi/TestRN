d31ca10c94c3bc127086aeda277e53de7d3bb16a
DivyaChitturi <divya.chitturi215@gmail.com>
2023-09-09 00:38:26 +0100

    Login Screen and Sign Up Screen

---
+++ RN_Project/Styles.js
@@ -0,0 +1,111 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DCDCDC',
+  },
+  inputContainer: {
+    borderBottomColor: '#F5FCFF',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 30,
+    borderBottomWidth: 1,
+    width: 300,
+    height: 45,
+    marginBottom: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+
+    shadowColor: '#808080',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  inputs: {
+    height: 45,
+    marginLeft: 16,
+    borderBottomColor: '#FFFFFF',
+    flex: 1,
+  },
+  inputIcon: {
+    width: 30,
+    height: 30,
+    marginRight: 15,
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: 300,
+    borderRadius: 30,
+    backgroundColor: 'transparent',
+  },
+  btnForgotPassword: {
+    height: 15,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    marginBottom: 10,
+    width: 300,
+    backgroundColor: 'transparent',
+  },
+  loginButton: {
+    backgroundColor: '#00b5ec',
+
+    shadowColor: '#808080',
+    shadowOffset: {
+      width: 0,
+      height: 9,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 12.35,
+
+    elevation: 19,
+  },
+  loginText: {
+    color: 'white',
+  },
+  bgImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+  },
+  btnText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  restoreButtonContainer: {
+    width: 250,
+    marginBottom: 15,
+    alignItems: 'flex-end',
+  },
+  socialButtonContent: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  socialIcon: {
+    color: '#FFFFFF',
+    marginRight: 5,
+  },
+  fabookButton: {
+    backgroundColor: '#3b5998',
+  },
+  googleButton: {
+    backgroundColor: '#ff0000',
+  },
+});
+
+export default styles;
---
+++ RN_Project/src/Screens/LoginScreen.js
@@ -0,0 +1,93 @@
+import React, {useState} from 'react';
+import {Text, View, TextInput, TouchableOpacity, Image} from 'react-native';
+import styles from '../../Styles';
+
+const LoginScreen = props => {
+  const [userName, setUserName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [isRegistering, setIsRegistering] = useState(false);
+
+  const loginHandler = () => {
+    console.log('loginHandler');
+  };
+
+  const registerHandler = () => {
+    console.log('isRegistering' + isRegistering);
+    if (!isRegistering) {
+      setIsRegistering(true);
+      //runOnJS(setIsRegistering)(true);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.bgImage}
+        source={require('/Users/itc-consultant/Documents/GitHub/TestRN/RN_Project/Assets/Images/HomeImage.jpeg')}
+      />
+      {isRegistering && (
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.inputs}
+            placeholder="userName"
+            value={userName}
+            underlineColorAndroid="transparent"
+            onChangeText={text => setUserName(text)}
+          />
+        </View>
+      )}
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputs}
+          value={email}
+          placeholder="Email"
+          keyboardType="email-address"
+          underlineColorAndroid="transparent"
+          onChangeText={text => setEmail(text)}
+        />
+        <Image
+          style={styles.inputIcon}
+          source={{uri: 'https://img.icons8.com/nolan/40/000000/email.png'}}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputs}
+          value={password}
+          placeholder="Password"
+          secureTextEntry={true}
+          underlineColorAndroid="transparent"
+          onChangeText={text => setPassword(text)}
+        />
+        <Image
+          style={styles.inputIcon}
+          source={{uri: 'https://img.icons8.com/nolan/40/000000/key.png'}}
+        />
+      </View>
+      <TouchableOpacity
+        style={styles.btnForgotPassword}
+        onPress={() => showAlert('restore_password')}>
+        <Text style={styles.btnText}>Forgot your password?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.buttonContainer, styles.loginButton]}>
+        <Text style={styles.loginText}>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={registerHandler}
+        //onPress={
+        //() => registerHandler
+        //   dispatch(
+        //     request({url: kApiSignup, data: {userName, email, password}}),
+        //   )
+        //}
+      >
+        <Text style={styles.btnText}>Sign Up</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default LoginScreen;
