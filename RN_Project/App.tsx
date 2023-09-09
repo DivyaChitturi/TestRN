712f4ee3d78ba93c21eaffaa51a9aee279120d66
DivyaChitturi <divya.chitturi215@gmail.com>
2023-09-09 01:42:39 +0100

    added stack navigators

--- RN_Project/App.tsx
+++ RN_Project/App.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native';
 
 import LoginScreen from './src/Screens/LoginScreen';
 
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const Nav = () => {
+  const [isULoggedIn, setIsULoggedIn] = useState(false);
+
+  const authStack = () => {
+    return (
+      <Stack.Group>
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      </Stack.Group>
+    );
+  };
+  const mainStack = () => {
+    return (
+      <Stack.Group>
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      </Stack.Group>
+    );
+  };
+
+  return (
+    <Stack.Navigator>{isULoggedIn ? mainStack() : authStack()}</Stack.Navigator>
+  );
+};
+
 function App(): JSX.Element {
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-      }}>
-      <LoginScreen />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Nav />
+    </NavigationContainer>
   );
 }
 
