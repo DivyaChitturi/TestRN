f3fce2e27e6c7aa1b86bade6a51c92fce56c9881
DivyaChitturi <divya.chitturi215@gmail.com>
2023-09-10 17:25:55 +0100

    Added Maps permissions

--- RN_Project/src/Screens/MapScreen.js
+++ RN_Project/src/Screens/MapScreen.js
@@ -1,56 +1,44 @@
-import {useRef, useState} from 'react';
+import {useRef, useEffect} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
-import MapView, {PROVIDER_GOOGLE, Marker, Callout} from 'react-native-maps';
-
-const markersArray = [
-  {lat: 0, lon: 0},
-  {lat: 0.5, lon: 0.5},
-  {lat: 1, lon: 1},
-  {lat: 1.5, lon: 1.5},
-  {lat: 2, lon: 2},
-  {lat: 2.5, lon: 2.5},
-];
+import MapControl from '../Controls/MapControl';
+import LocationHelper from '../Helpers/LocationHelper';
 
 const MapScreen = () => {
-  const mapRef = useRef(null);
+  useEffect(() => {
+    LocationHelper.checkLocationPermission(
+      () => {
+        LocationHelper.trackUserLocation(
+          locationObject => {
+            console.log(locationObject);
 
-  const renderMarkers = () => {
-    return markersArray.map((item, index) => {
-      return (
-        <Marker coordinate={{latitude: item.lat, longitude: item.lon}}>
-          <View
-            style={{
-              width: 40,
-              height: 40,
-              borderRadius: 20,
-              backgroundColor: 'red',
-            }}>
-            <Text>a</Text>
-          </View>
-        </Marker>
-      );
-    });
-  };
+            if (locationObject.coords) {
+              parentControlMapRef.current.animateToCustomLocation({
+                latitude: locationObject.coords.latitude,
+                longitude: locationObject.coords.longitude,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121,
+              });
+            }
+          },
+          error => {},
+        );
+      },
+      () => {},
+    );
+  }, []);
 
-  //37.3346437,-122.0138429
+  const parentControlMapRef = useRef(null);
   return (
     <View style={{flex: 1}}>
       <View style={{flex: 1}}>
         <Text>maps</Text>
-        <MapView
-          // provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          style={{flex: 1}}
-          ref={mapRef}
-          showsUserLocation
-          showsMyLocationButton>
-          {renderMarkers()}
-        </MapView>
+        <MapControl ref={parentControlMapRef} style={{flex: 1}} />
       </View>
       <View style={{flexDirection: 'row'}}>
         <View style={{flex: 1}}>
           <TouchableOpacity
             onPress={() => {
-              mapRef.current.animateToRegion({
+              parentControlMapRef.current.animateToCustomLocation({
                 latitude: 37.3346437,
                 longitude: -122.0138429,
                 latitudeDelta: 0.015,
@@ -58,27 +46,24 @@ const MapScreen = () => {
               });
             }}
             style={{
-              position: 'absolute',
               left: 10,
               right: 10,
-              bottom: 20,
+              bottom: 30,
               backgroundColor: 'green',
               borderRadius: 15,
               justifyContent: 'center',
               alignItems: 'center',
-              width: 98,
-              height: 98,
+              width: 70,
+              height: 70,
               margin: 4,
-              alignSelf: 'flex-end',
             }}>
-            <Text>Apple HeadQuarters</Text>
+            <Text>Apple HQ</Text>
           </TouchableOpacity>
         </View>
         <View style={{flex: 1}}>
           <TouchableOpacity
             onPress={() => {
-              //51.5260337,-0.0880577
-              mapRef.current.animateToRegion({
+              parentControlMapRef.current.animateToCustomLocation({
                 latitude: 51.5260337,
                 longitude: -0.0880577,
                 latitudeDelta: 0.015,
@@ -86,18 +71,16 @@ const MapScreen = () => {
               });
             }}
             style={{
-              position: 'absolute',
               left: 10,
               right: 10,
-              bottom: 20,
+              bottom: 30,
               backgroundColor: 'yellow',
               borderRadius: 15,
               justifyContent: 'center',
               alignItems: 'center',
-              width: 98,
-              height: 98,
+              width: 70,
+              height: 70,
               margin: 4,
-              alignSelf: 'flex-center',
             }}>
             <Text>ITC Office</Text>
           </TouchableOpacity>
@@ -105,8 +88,7 @@ const MapScreen = () => {
         <View style={{flex: 1}}>
           <TouchableOpacity
             onPress={() => {
-              //27.1751495,78.0395619
-              mapRef.current.animateToRegion({
+              parentControlMapRef.current.animateToCustomLocation({
                 latitude: 27.1751495,
                 longitude: 78.0395619,
                 latitudeDelta: 0.015,
@@ -114,18 +96,16 @@ const MapScreen = () => {
               });
             }}
             style={{
-              position: 'absolute',
               left: 10,
               right: 10,
-              bottom: 20,
+              bottom: 30,
               backgroundColor: 'pink',
               borderRadius: 15,
               justifyContent: 'center',
               alignItems: 'center',
-              width: 98,
-              height: 98,
+              width: 70,
+              height: 70,
               margin: 4,
-              alignSelf: 'flex-start',
             }}>
             <Text>TajMahal</Text>
           </TouchableOpacity>
